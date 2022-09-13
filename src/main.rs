67ea2d8a1712cff28d7cbd059eca9899a08dfabc
Zachary S <zasample18+github@gmail.com>
2022-09-13 17:05:12 -0500

    Add preliminary support for background colors.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use std::io::{self, prelude::*, BufReader, BufWriter};
 
 #[derive(Clone)]
 struct Color{
-    hue: u16,
+    fg_hue: Option<u16>,
+    bg_hue: Option<u16>,
 }
 
 impl Color {
     fn add(&mut self, value: i32) {
-        self.hue = ((self.hue as i32 + value) % 1530) as u16;
+        if let Some(hue) = &mut self.fg_hue {
+            *hue = ((*hue as i32 + value) % 1530) as u16;
+        }
+        if let Some(hue) = &mut self.bg_hue {
+            *hue = ((*hue as i32 + value) % 1530) as u16;
+        }
     }
 }
 
@@ -23,8 +29,16 @@ fn hue_to_color(mut hue: u16) -> [u8; 3] {
 
 impl std::fmt::Display for Color {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let channels = hue_to_color(self.hue);
-        f.write_fmt(format_args!("\x1b[38;2;{};{};{}m", channels[0], channels[1], channels[2]))
+        if let Some(hue) = self.fg_hue {
+            let channels = hue_to_color(hue);
+            f.write_fmt(format_args!("\x1b[38;2;{};{};{}m", channels[0], channels[1], channels[2]))?;
+
+        }
+        if let Some(hue) = self.bg_hue {
+            let channels = hue_to_color(hue);
+            f.write_fmt(format_args!("\x1b[48;2;{};{};{}m", channels[0], channels[1], channels[2]))?;
+        }
+        Ok(())
     }
 }
 
@@ -45,7 +59,7 @@ fn main() {
 
     let mut outfile: Box<dyn Write> = Box::new(BufWriter::new(io::stdout().lock()));
 
-    let mut line_start_color = Color { hue: 0 };
+    let mut line_start_color = Color { fg_hue: Some(0), bg_hue: None };
 
     for line in infile.lines() {
         let mut char_color = line_start_color.clone();
